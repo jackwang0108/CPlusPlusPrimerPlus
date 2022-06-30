24a14ebd6bb9cb187ed6d39d327ac2a6e4b586e0
JackWang <jack4shihong@gmail.com>
2022-06-30 16:39:57 +0800

    finished chapter10

---
+++ chapter10/include/stock10.h
@@ -0,0 +1,25 @@
+#ifndef STOCK10_H_
+#define STOCK10_H_
+
+#include <string>
+
+class Stock
+{
+private:
+    std::string company;
+    long shares;
+    double share_val;
+    double total_val;
+    void set_tot() { total_val = shares * share_val; }
+
+public:
+    Stock();
+    Stock(const std::string &co, long n, double pr);
+    ~Stock();
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show();
+};
+
+#endif
