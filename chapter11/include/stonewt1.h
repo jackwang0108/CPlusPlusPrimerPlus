092f32f2166e3bb81ada6b81dfdd7e0afe6b5809
JackWang <jack4shihong@gmail.com>
2022-07-01 13:08:54 +0800

    finished chapter11

--- chapter11/include/stonewt1.h
+++ chapter11/include/stonewt1.h
@@ -1,4 +1,26 @@
 #ifndef STONEWT1_H_
 #define STONEWT1_H_
 
+class Stonewt {
+private:
+    enum {Lbs_per_stn = 14};
+    int stone;
+    double pds_left;
+    double pounds;
+
+public:
+    Stonewt();
+    Stonewt(double lbs);
+    Stonewt(int stn, double lbs);
+    ~Stonewt();
+
+    void show_lbs() const;
+    void show_stn() const;
+
+    // convertion
+    operator int() const;
+    // 使用explict禁止arrary[Stonewt(1.2)]这样的隐式转换
+    explicit operator double() const;
+};
+
 #endif
