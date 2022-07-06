06efa944e5d8a51139e2dcd7ad022e7bd4a1d4f9
JackWang <jack4shihong@gmail.com>
2022-07-06 14:00:23 +0800

    finished part of chapter15

---
+++ chapter15/error2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cfloat>
+
+bool hmean(double a, double b, double *ans);
+
+int main(void){
+    double x, y, z;
+
+    std::cout << "Enter two numbers: ";
+    while (std::cin >> x >> y){
+        if (hmean(x, y, &z))
+            std::cout << "Harmonic mean of " << x << " and " << y << " is " << z << std::endl;
+        else
+            std::cout << "One value should not be the negative of the other - try again.\n";
+        std::cout << "Enter next set of number <q to quit>: ";
+    }
+    std::cout << "Bye!\n";
+    return 0;
+}
+
+bool hmean(double a, double b, double *ans){
+    if (a == -b){
+        *ans = DBL_MAX;
+        return false;
+    }
+    *ans = 2.0 * a * b / (a + b);
+    return true;
+}
